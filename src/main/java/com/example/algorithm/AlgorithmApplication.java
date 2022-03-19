7b2fc55219a58820bcca99ec898b9bc62ff59e47
mac <xxsdxmd@163.com>
2022-03-19 21:08:57 +0800

    feat: 1.初始化项目

---
+++ src/main/java/com/example/algorithm/AlgorithmApplication.java
@@ -0,0 +1,17 @@
+package com.example.algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xxs
+ * 算法学习
+ */
+@SpringBootApplication
+public class AlgorithmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmApplication.class, args);
+    }
+
+}
